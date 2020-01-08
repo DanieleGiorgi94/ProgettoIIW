778028c899ca462febad929380b4f435322c0205
DanieleGiorgi94 <giorgi.daniele@icloud.com>
2020-01-08 11:42:54 +0100

    Aggiornamento 08/01/20

--- code/client/client.c
+++ code/client/client.c
@@ -158,11 +158,13 @@ int main(int argc, char **argv)
             fprintf(stderr, "Usage: GET <filename.format>\n");
             goto RESET;
         } else {
-            if (check_file(token_vector[1], list_dir(PATH))) {
 
-                char *filepath = dynamic_allocation(sizeof(char *)*BUFLEN);
+            char *filepath = dynamic_allocation(sizeof(char *)*BUFLEN);
+            filepath = obtain_path(filepath, token_vector[1], token_vector[0]); // ottengo path_assoluto file
 
-                filepath = obtain_path(filepath, token_vector[1], token_vector[0]); // ottengo path_assoluto file
+
+            if (check_file(token_vector[1], list_dir(filepath)) {
+
 
                 /* TODO: Il client manda un pacchetto al server con dentro il nome del file (token_vector[1]) che deve
                  * trovare nella sua cartella di upload, mentre download sarà per la put dei file. Se il file esiste,
